d14d9fbf3f2fff6e8e880b32a13100e1f14d954c
Pellizon <lhgp09@gmail.com>
2017-06-25 13:59:03 -0300

    Algoritmo em P para o Clique (guloso)
    Fonte: https://stackoverflow.com/questions/22641528/flaw-in-this-maximum-clique-polynomial-time-approach
    Entrada modificada para se adaptar as entradas que já temos
    
    Ideia:
    1. Consider a node as permanent node and form a set of nodes such that each node is connected to this permanent node.(the set includes permanent node as well)
    2. Now form a subgraph of the original graph such that it contains all the nodes in the set formed and only those edges which are between the nodes present in the set.
    3. Find degree of each node.
    4. If all the nodes have same degree then we have a clique.
    5. Else delete the least degree node from this subgraph and repeat from step 3.
    6. Repeat step 1-5 for all the nodes in the graph.

---
+++ final_alg/P_max_clique.cpp
@@ -0,0 +1,111 @@
+//
+//  max_clique_p.cpp
+//  WS_C
+//
+//  Created by Lucas Pellizon on 6/24/17.
+//  Copyright © 2017 Lucas Pellizon. All rights reserved.
+//
+
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+#include<cstring>
+#include<stdlib.h>
+#include<algorithm>
+#include<limits.h>
+#include<list>
+using namespace std;
+int **adj,m,*s;
+int clique(int s[],int l)
+{
+    int i,j,deg[l],mini = 0;
+    while(1)
+    {
+        int minn=l+5,maxx=0;
+        memset(deg,0,sizeof(deg));
+        for(i=0;i<l;i++)
+        {
+            for(j=0;j<l;j++)
+            {
+                if(adj[s[i]][s[j]] && i!=j)
+                {
+                    deg[i]++;
+                    //             cout<<"i="<<s[i]<<" j="<<s[j]<<endl;
+                }
+            }
+            
+        }
+        /*    cout<<"deg==>"<<endl;
+         for(i=0;i<l;i++)
+         cout<<deg[i]<<" ";
+         cout<<endl;
+         */    for(i=0;i<l;i++)
+         {
+             if(deg[i]<minn)
+                 minn=deg[i],mini=i;
+             if(deg[i]>maxx)
+                 maxx=deg[i];
+         }
+        //    cout<<"min="<<minn<<" max="<<maxx<<endl;
+        //   system("pause");
+        if(minn==maxx)
+            return l;
+        else
+        {
+            l--;
+            for(i=1;i<mini;i++)
+                s[i]=s[i];
+            for(;i<=l;i++)
+                s[i]=s[i+1];
+        }
+    }
+}
+int main()
+{
+    int m,i,j,maxx=0;
+    int v, e;
+    int x, y;
+    scanf("%d", &v);//Número de vértices
+    scanf("%d", &m); //Número de arestas
+    
+    s = (int*) malloc(m*sizeof(int));
+    adj = (int**) malloc(m*sizeof(int*));
+    for (i=0;i<m;i++){
+        adj[i] = (int*) malloc(m*sizeof(int));
+
+    }
+
+    
+    for(int i=0;i<m;i++){
+        cin>>x;
+        cin>>y;
+//        grauvertice[x]++;
+//        grau_vertice[y]++;
+        adj[x][y] = adj[y][x] = 1; //Matriz de adjacência
+    }
+//    cin>>m;
+//    for(i=0;i<m;i++)
+//    {
+//        for(j=0;j<m;j++)
+//            cin>>adj[i][j];
+//    }
+    /*  for(i=0;i<m;i++)
+     {
+     for(j=0;j<m;j++)
+     cout<<adj[i][j]<<" ";
+     cout<<endl;
+     }
+     */
+    for(i=0;i<m;i++)
+     {
+         int s[867]={i},len=1;
+         //    cout<<"i===>"<<i+1<<endl;
+         for(j=0;j<m;j++)
+             if(adj[i][j])
+                 s[len++]=j;//,cout<<j+1<<" ";
+         //  cout<<"len="<<len<<endl;
+         maxx=max(maxx,clique(s,len));
+     }
+    cout<<"clique maximo: " << maxx << "\n";
+    return 0;
+}
